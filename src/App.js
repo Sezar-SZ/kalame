11f80f320114af0a66ea0c150021f5a8ae05bdec
Sezar-SZ <soheilzarepoor79@gmail.com>
2022-03-28 09:39:35 +0430

    get a random word from txt file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import txt_words from "./5-letters-words.txt";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [word, setWord] = useState("");
+
+    const getNewWord = async () => {
+        try {
+            const res = await fetch(txt_words);
+            const data = await res.text();
+
+            const words = data.split("\n");
+            const randomIndex = Math.floor(Math.random() * words.length);
+            const randomWord = words[randomIndex].trim();
+            setWord(randomWord);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    useEffect(() => {
+        getNewWord();
+    }, []);
+
+    useEffect(() => {
+        console.log(word);
+    }, [word]);
+
+    return (
+        <div className="App">
+            <h2>hello word</h2>
+            <h2>{word}</h2>
+        </div>
+    );
+};
 
 export default App;
