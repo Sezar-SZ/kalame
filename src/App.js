6ea29bd159f56243933f7ac946ddc3c2c9e1dc51
Sezar-SZ <soheilzarepoor79@gmail.com>
2022-03-28 20:27:16 +0430

    keyboard functionality, enter and backspace

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import txt_words from "./5-letters-words.txt";
 import { useEffect, useState } from "react";
 import "./App.css";
+import useKeyPress from "./hooks/useKeyPress";
 
 const App = () => {
     const [word, setWord] = useState("");
@@ -12,6 +13,34 @@ const App = () => {
         ["", "", "", "", ""],
         ["", "", "", "", ""],
     ]);
+    const [currentRow, setCurrentRow] = useState(0);
+
+    const getNextLetterIndex = () => {
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 5; j++) {
+                if (gameMap[i][j] === "") {
+                    return [i, j];
+                }
+            }
+        }
+        return -1;
+    };
+    const getLastLetterIndex = () => {
+        for (let i = 5; i >= 0; i--) {
+            for (let j = 4; j >= 0; j--) {
+                if (gameMap[i][j] !== "") return [i, j];
+            }
+        }
+        return -1;
+    };
+
+    const getGuessedWord = (row) => {
+        let guessedWord = "";
+        gameMap[row].forEach((letter) => {
+            guessedWord += letter;
+        });
+        return guessedWord;
+    };
 
     const getNewWord = async () => {
         try {
@@ -31,6 +60,31 @@ const App = () => {
         getNewWord();
     }, []);
 
+    useKeyPress((key) => {
+        if (key.length === 1 && key !== " ") {
+            let newGameMap = [...gameMap];
+            const letterIndex = getNextLetterIndex();
+            if (letterIndex !== -1 && letterIndex[0] === currentRow) {
+                newGameMap[letterIndex[0]][letterIndex[1]] = key;
+                setGameMap(newGameMap);
+            }
+        } else if (key === "Backspace") {
+            let newGameMap = [...gameMap];
+            const letterIndex = getLastLetterIndex();
+            if (letterIndex !== -1 && letterIndex[0] === currentRow) {
+                newGameMap[letterIndex[0]][letterIndex[1]] = "";
+            }
+        } else if (key === "Enter") {
+            if (!gameMap[currentRow].includes("") && currentRow <= 5) {
+                const guessedWord = getGuessedWord(currentRow);
+                setCurrentRow(currentRow + 1);
+                console.log("enter accepted, word is", guessedWord);
+                // logic of the game...
+                // for styles can have an array of classnames for each cell
+            }
+        }
+    });
+
     useEffect(() => {
         console.log(word);
     }, [word]);
@@ -38,7 +92,7 @@ const App = () => {
     return (
         <div className="App">
             <div className="header">
-                <h1>کلمه</h1>
+                <h1>کـلــمــه</h1>
             </div>
             <div className="game">
                 <div className="map">
